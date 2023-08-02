c0bc8635b08d512de2a1e3d3d020c8af5e1328d1
Atreus <72207991+H4ppyCode@users.noreply.github.com>
2023-08-02 11:20:21 +0200

    Add files via upload
    add bot

---
+++ bot.js
@@ -0,0 +1,37 @@
+require("dotenv").config(); // Importez et configurez dotenv pour charger les variables d'environnement
+
+const { Client, GatewayIntentBits, REST, Routes } = require("discord.js");
+
+const bot = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
+
+console.log("Connexion au bot...");
+
+bot
+  .login(process.env.DISCORD_TOKEN) // Utilisez votre token depuis les variables d'environnement
+  .then(() => console.log("Connecté au bot !"))
+  .catch((error) =>
+    console.log("Impossible de se connecter au bot - " + error)
+  );
+
+bot.once("ready", async () => {
+  console.log("Le bot est prêt !");
+
+  const commands = [
+    {
+      name: "badge",
+      description: "oue le badge bouffon !",
+    },
+  ];
+
+  const commandData = await bot.application.commands.set(commands);
+  console.log("Commandes enregistrées !", commandData);
+});
+
+bot.on("interactionCreate", (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === "badge")
+    interaction.reply("oue le badge bouffon!");
+});
